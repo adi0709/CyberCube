717fef2a00ed18303f81fc1c09cab68fade0b69d
Aditya Panwar <adityapanwar@lap-adpanw.local>
2024-03-28 12:09:26 +0200

    Added record functionality to tests to Cypress Dshboard

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/*.feature",
+      projectId: "f2w5tr",
+      // ...rest of the Cypress project config
     chromeWebSecurity: false,
     "html": {
       "enabled": true
